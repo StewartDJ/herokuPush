4a247da8b1daf69cf1eeea3bc5a80ec8459c7ffa
DJ Stewart <deshastewart1992@gmail.com>
2020-09-09 08:33:14 -0700

    commit

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require('express')
 const cors = require('cors') 
 const mongoose = require('mongoose')
 
-require('dotenv').config();
 
 const app = express();
+require('dotenv').config();
+
 const port = process.env.PORT ||  5000;
+
 
 app.use(cors());
 app.use(express.json()); 
